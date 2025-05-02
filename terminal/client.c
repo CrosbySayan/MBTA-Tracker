8593dfb8ce031782c568a85ae79914fa4f45da32
Crosby <crosbysayan2023@gmail.com>
2025-05-02 16:03:46 -0400

    Pulling and Parseing online - now need to compile data and add call for stop pos

--- terminal/client.c
+++ terminal/client.c
@@ -1,19 +1,111 @@
+#include "Libs/cJSON.h"
 #include <curl/curl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #define HTTPS_Port "443"
+
+typedef struct {
+  char *id;
+  double TTA;
+  char *current_stop;
+  int num_stops_away;
+  char *process;
+} Vehichle;
+
+/**
+ * Helper function that translates ISO time into seconds from current
+ */
+time_t ISO_to_Seconds(const char *iso_timestamp) {
+  struct tm tm = {0};
+  int year, month, day, hour, minute, second;
+  int tz_hour = 0, tz_minute = 0;
+  char tz_sign = '+';
+
+  // Parse the main part of the timestamp
+  sscanf(iso_timestamp, "%d-%d-%dT%d:%d:%d", &year, &month, &day, &hour,
+         &minute, &second);
+
+  // Parse timezone info if present
+  const char *tz_part = strchr(iso_timestamp, '+');
+  if (!tz_part) {
+    tz_part = strchr(iso_timestamp, '-');
+    if (tz_part &&
+        tz_part > iso_timestamp + 10) { // Make sure it's not the date hyphen
+      tz_sign = '-';
+    } else {
+      tz_part = NULL;
+    }
+  }
+
+  if (tz_part) {
+    sscanf(tz_part + 1, "%d:%d", &tz_hour, &tz_minute);
+  }
+
+  // Set up the tm structure
+  tm.tm_year = year - 1900; // Years since 1900
+  tm.tm_mon = month - 1;    // Months 0-11
+  tm.tm_mday = day;
+  tm.tm_hour = hour;
+  tm.tm_min = minute;
+  tm.tm_sec = second;
+  tm.tm_isdst = -1; // Let the system determine DST
+
+  // Convert to time_t (seconds since epoch)
+  time_t timestamp_time = mktime(&tm);
+
+  // Apply timezone offset (convert from local to UTC)
+  int tz_offset_seconds = tz_hour * 3600 + tz_minute * 60;
+  if (tz_sign == '+') {
+    timestamp_time -= tz_offset_seconds;
+  } else {
+    timestamp_time += tz_offset_seconds;
+  }
+
+  return timestamp_time;
+}
+
+double Seconds_from_Current(const char *timestamp) {
+  time_t arr_time = ISO_to_Seconds(timestamp);
+  time_t current_time;
+  time(&current_time);
+
+  return difftime(arr_time, current_time);
+}
+/**
+ * Turns our JSON request of a given prediction into human readable format.
+ */
+char *processJSON(cJSON *object) {
+  cJSON *attributes = cJSON_GetObjectItemCaseSensitive(object, "attributes");
+  cJSON *arrTime = cJSON_GetObjectItemCaseSensitive(attributes, "arrival_time");
+  fprintf(stderr, "%s\n", arrTime->valuestring);
+  double time = Seconds_from_Current(arrTime->valuestring);
+  fprintf(stderr, "%.2f SECS\n", time);
+  fprintf(stderr, "%.2f MINS\n", (time / 60));
+  return NULL;
+}
 
 size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
   size_t real_size = size * nmemb;
-  printf("%.*s", (int)real_size, ptr);
-  printf("\n");
+  // printf("%.*s", (int)real_size, ptr);
+
+  cJSON *json = cJSON_Parse(ptr);
+  // printf("\n");
+  fprintf(stderr, "%s\n", cJSON_Print(json));
+  cJSON *allObjects = cJSON_GetObjectItemCaseSensitive(json, "data");
+  cJSON *object = NULL;
+
+  cJSON_ArrayForEach(object, allObjects) { processJSON(object); }
   return real_size;
 }
 
 int main(int argc, char *argv[]) {
-
+  // 1 means inbound 0 means outbound
   /**
    * ToDo:
    * Build client side that can connect to HTTPS server
-   * Port: 443
+   * Trains are inconsistent, provide stop data as well for accurate gauging
    */
   // curl_easy_setopt(); curl struct, type (CURLOPT_URL), request
   CURL *curl;
@@ -25,5 +117,9 @@ int main(int argc, char *argv[]) {
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
     res = curl_easy_perform(curl);
   }
+
+  // Every Minute Send in One Request (1)
+  // Grab two closest T's and two closest Buses and order them by time
+  // Then call the each Vehichle to get stop information
   return 0;
 }
