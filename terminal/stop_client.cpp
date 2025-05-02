5f6d641e21a161395e9caf81342d18b492ade1ea
Crosby <crosbysayan2023@gmail.com>
2025-05-02 01:28:36 -0400

    Curl start with pivot to custom
    I'm going to start with curl for rapid, development but I want to pivot
    to a custom HTTPS request for further experience in networking. Also it
    makes sense to use the library right now because the ESP32 has a lib to

---
+++ terminal/stop_client.cpp
@@ -0,0 +1,10 @@
+#include <curl/curl.h>
+#define HTTPS_Port "443"
+int main(int argc, char *argv[]) {
+  /**
+   * ToDo:
+   * Build client side that can connect to HTTPS server
+   * Port: 443
+   */
+  return 0;
+}
